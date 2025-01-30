ee6da0b05863e9a7c79e501f33827f0173a23a44
davidkonrad <davidkonrad@gmail.com>
2025-01-30 11:34:00 +0100

    work on layout

--- playground.js
+++ playground.js
@@ -5,28 +5,12 @@ const Playground = (function() {
 	const gebi = (id) => { return document.getElementById(id) }
 	const qsel = (sel) => { return document.querySelector(sel) }
 	const qall = (sel) => { return document.querySelectorAll(sel) }
+	const default_iconSize = '1.7rem'
 	let editor = undefined
 	
 	const init = function() {
 		initForm()
 		updateEditor()
-	}
-
-	const getDefaults = function() {
-		const groups = ['Smileys & Emotion', 'Activities', 'Animals & Nature', 'Flags', 'Food & Drink', 'Objects', 'People & Body', 'Symbols', 'Travel & Places']
-		return {
-			groups: groups.slice(),
-			names: groups.slice(),
-			favorites: true,
-			iconSize: '1.5rem',
-			skinTone: 'neutral',
-			topmenu: {
-				search: false,
-				skinTone: false,
-				iconSize: false
-			},
-			showFallbacks: false
-		}
 	}
 
 	const storage = function(o) {
@@ -42,40 +26,36 @@ const Playground = (function() {
 		}
 	}
 
-	function DragDrop(cnt, cls) {
-		const items = cnt.querySelectorAll(cls)
-		const name = cnt.getAttribute('name')
+	const initOrderable = function(cnt, name) {
+		const items = cnt.querySelectorAll('.item-sortable')
 		let dragged = undefined
 		let allowDrop = undefined
 		for (const item of items) {
 			item.draggable = true
+			item.name = name
 			item.ondragstart = function(e) {
 				dragged = this
 				e.dataTransfer.dropEffect = 'move'
 				e.dataTransfer.effectAllowed = 'move'
 				e.dataTransfer.setData('text/html', item.innerHTML)
-
-				e.dataTransfer.setData('checked', item.querySelector('input[type="checkbox"]').checked)
-				e.dataTransfer.setData('value', item.querySelector('input[type="text"]').value)
-
-				//checked = item.querySelector('input[type="checkbox"]').checked
+				if (item.name === 'groups-names') {
+					e.dataTransfer.setData('checked', item.querySelector('input[type="checkbox"]').checked)
+					e.dataTransfer.setData('value', item.querySelector('input[type="text"]').value)
+				}
 			}
 			item.ondragover = function(e) {
-				const el = document.elementFromPoint(e.clientX, e.clientY)
 				e.preventDefault()
-				const n = el.parentElement.parentElement.getAttribute('name') //this.getAttribute('data-name')
-				allowDrop = n === name
+				allowDrop = true //!!?? must find a way to handle illegal drops
 			}
 			item.ondrop = function(e) {
 				e.preventDefault()
 				if (dragged !== item && allowDrop) {
 					dragged.innerHTML = item.innerHTML
 					item.innerHTML = e.dataTransfer.getData('text/html')
-				
-					item.querySelector('input[type="checkbox"]').checked = e.dataTransfer.getData('checked')
-					item.querySelector('input[type="text"]').value = e.dataTransfer.getData('value')
-
-					//item.querySelector('input[type="checkbox"]').checked = checked
+					if (item.name === 'groups-names') {
+						item.querySelector('input[type="checkbox"]').checked = e.dataTransfer.getData('checked')
+						item.querySelector('input[type="text"]').value = e.dataTransfer.getData('value')
+					}
 				}
 			}
 			item.ondragenter = function(e) {
@@ -85,42 +65,13 @@ const Playground = (function() {
 				item.classList.remove('active')
 			}
 			item.ondragend = function() {
-				for (const item of items) { 
-					item.classList.remove('active')
-				}
+				for (const item of items) item.classList.remove('active')
 				initGroups()
-			}
-		} //(cnt, name, dragged, items, checked, allowDrop)
-	}
-
-/*
-	const initGroups = function() {
-		const cnt = qsel('div[name="groups-names"]')
-		cnt.querySelectorAll('input[type="checkbox"]').forEach(function(input) {
-			input.onclick = function() {
-				const name = this.parentElement.nextElementSibling 
-				if (!this.checked) {
-					name.setAttribute('disabled', 'disabled')
-					this.parentElement.style.color = 'rgb(170, 170, 170)'
-				} else {
-					name.removeAttribute('disabled')
-					this.parentElement.style.color = '#333'
-				}
 				updateEditor()
 			}
-		})
-		cnt.querySelectorAll('input[type="text"]').forEach(function(input) {
-			input.onfocus = function() {
-				this.oldValue = this.value
-				this.parentElement.draggable = false
-			}
-			input.onblur = function() {
-				this.parentElement.draggable = true
-				if (this.oldValue !== this.value) updateEditor()
-			}
-		})
+		}
 	}
-*/
+
 	const initGroups = function() {
 		const cnt = qsel('#option-groups-names div')
 		cnt.querySelectorAll('input[type="checkbox"]').forEach(function(input) {
@@ -162,34 +113,57 @@ const Playground = (function() {
 				</div>`
 			div.insertAdjacentHTML('beforeend', html)
 		}
-		new DragDrop(div, '.item-sortable')
-
-		//--------------------------------------------
-/*
-		const cnt = qsel('div[name="groups-names"]')
-		//const groups = ['Smileys & Emotion', 'Activities', 'Animals & Nature', 'Flags', 'Food & Drink', 'Objects', 'People & Body', 'Symbols', 'Travel & Places']
-		for (const group of groups) {
-			const html = `
-				<div class="item-sortable bb" style="display:table-row;">
-					<label class="fieldset-item" style="display: table-cell; ">
-						<input type="checkbox" name="${group}" checked>${group}</label>
-					</label>
-					<input class="fieldset-item" type="text" name="${group}" value="${group}" style="display:table-cell;" >
-				</div>`
-			cnt.insertAdjacentHTML('beforeend', html)
-		}
-*/
-		//groups
+		initOrderable(div, 'groups-names')
 		initGroups()
-		//new DragDrop(cnt, '.bb')
 
-		//topmenu
 		qsel('#option-topmenu div').querySelectorAll('input[type="checkbox"]').forEach(function(input) {
 			input.onclick = function() {
 				updateEditor()
 			}
 		})
-		new DragDrop(qsel('#option-topmenu div'), '.item-sortable')
+		initOrderable(qsel('#option-topmenu div'), 'topmenu')
+
+		qsel('#option-recent input').onclick = function() {
+			updateEditor()
+		}
+
+		const updateCurrent = function(current) {
+			gebi('iconsize-current').innerText = '(' +  current +')'
+		}
+		const select = gebi('select-iconsize')
+		let size = 0.3
+		while (size < 5) {
+			const option = document.createElement('option')
+			const fs = parseFloat(size).toPrecision(2) + 'rem'
+			option.value = fs
+			option.style.fontSize = fs
+			option.innerText = '😊'
+			option.title = fs
+			if (fs === default_iconSize) {
+				option.selected = true
+				updateCurrent(fs)
+			}			
+			select.appendChild(option)
+			size += 0.2
+		}
+		select.onchange = function() {
+			updateCurrent(this.value)
+			updateEditor()
+		}		
+
+		qall('#option-skinTone input[name="skinTone"]').forEach(function(input) {
+			input.onclick = function() {
+				updateEditor()
+			}
+		})
+
+		qsel('#option-showFallbacks input').onclick = function() {
+			updateEditor()
+		}
+
+		qsel('#option-tagName select').onchange = function() {
+			updateEditor()
+		}
 
 		qall('aside details').forEach(function(d) {
 			d.ontoggle = function() {
@@ -205,7 +179,6 @@ const Playground = (function() {
 			const obj = { emojis: options }
 			s = JSON.stringify(obj, null, '  ')
 			s = s.replace(/"([^"]+)":/g, '$1:') //https://stackoverflow.com/a/11233515/1407478
-			//s = s.substr(1, s.length - 2)
 		}
 		gebi('options').innerText = s.trimLeft()
 	}
@@ -227,9 +200,13 @@ const Playground = (function() {
 				options.topmenu[input.name] = true
 			}
 		})
-		//console.log(options)
+		options.skinTone = qsel('#option-skinTone input[name="skinTone"]:checked').value
+		options.iconSize = qsel('#option-iconSize select').value
+		options.recent = qsel('#option-recent input').checked
+		options.showFallbacks = qsel('#option-showFallbacks input').checked
+		options.tagName = qsel('#option-tagName select').value
+
 		updateOptions(options)
-		//return options
 		initEditor(options)
 	}
 
@@ -240,7 +217,7 @@ const Playground = (function() {
 			mode: 'classic',
 			width: '100%',
 			height: 'auto',
-			minHeight : '40vh',
+			minHeight : '30vh',
 			plugins: [emojisPlugin],
 			buttonList: [
 				['font', 'fontSize', 'formatBlock'], ['emojis'],
